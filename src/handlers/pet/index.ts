ee94d7c9b34d073c772d803b9a2902101900897a
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-04-06 11:13:16 -0300

    feat: add pet handlers to routes

---
+++ src/handlers/pet/index.ts
@@ -0,0 +1,4 @@
+export { default as CreatePetHandler } from './create'
+export { default as ListPetsHandler } from './list'
+export { default as ShowPetHandler } from './show'
+export { default as DeletePetHandler } from './delete'
--- src/routes/pet.ts
+++ src/routes/pet.ts
@@ -1,11 +1,48 @@
+import { PrismaClient } from '@prisma/client'
 import { Router } from 'express'
+import { PetRepository, ShelterRepository } from '../repositories'
+import {
+    CreatePetRequestValidator,
+    UpdatePetRequestValidator,
+} from '../handlers/pet/request'
+import {
+    CreatePetHandler,
+    DeletePetHandler,
+    ListPetsHandler,
+    ShowPetHandler,
+} from '../handlers/pet'
+import { emptyBody } from '../middlewares'
+import UpdatePetHandler from '../handlers/pet/update'
 
 const router = Router()
 
-router.get('/pet/:id')
-router.post('/pet')
-router.put('/pet/:id')
-router.delete('/pet/:id')
-router.get('/pets')
+const db = new PrismaClient()
+
+const petRepository = new PetRepository(db)
+const shelterRepository = new ShelterRepository(db)
+
+const createShelterValidator = new CreatePetRequestValidator()
+const createHandler = new CreatePetHandler(
+    createShelterValidator,
+    petRepository,
+    shelterRepository
+)
+
+const updateShelterValidator = new UpdatePetRequestValidator()
+const updateHandler = new UpdatePetHandler(
+    updateShelterValidator,
+    petRepository,
+    shelterRepository
+)
+
+const deleteHandler = new DeletePetHandler(petRepository)
+const showHandler = new ShowPetHandler(petRepository)
+const listHandler = new ListPetsHandler(petRepository)
+
+router.get('/pet', (req, res) => showHandler.route(req, res))
+router.post('/pet', emptyBody, (req, res) => createHandler.route(req, res))
+router.delete('/pet', (req, res) => deleteHandler.route(req, res))
+router.put('/pet', emptyBody, (req, res) => updateHandler.route(req, res))
+router.get('/pets', (req, res) => listHandler.route(req, res))
 
 export default router
